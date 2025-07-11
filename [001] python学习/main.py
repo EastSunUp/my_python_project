6b8183aac7b002439f3f2d5c1b4bcd97258df759
EastSunUp <manleab.com>
2025-07-11 19:28:55 +0800

    python 旧文件添加注释 第1次 commit

---
+++ [001] python学习/main.py
@@ -0,0 +1,17 @@
+
+# This is a sample Python script.
+
+# Press Ctrl+F5 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Press the green button in the gutter to run the script.
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+import numpy
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press F9 to toggle the breakpoint.
+
+print_hi('pycharm')
+
+
