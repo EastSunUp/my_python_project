71d7f14ba5c435384d0c1f1efdd3e28fb513ab44
EastSunUp <manleab.com>
2026-01-05 02:41:49 +0800

    提交部分功能文件
    1.数据查询渠道(官方数据查询渠道)
    2.pip配置清华镜像源的方法(变更文件名称)
    3.计时器,获取时间

--- Timer_Acquirer.py
+++ Timer_Acquirer.py
@@ -1,40 +1,38 @@
 
+
 # TODO 文件需要commit
 import time
 
-class Hello:
-    def print_hi(self,name):
-        # Use a breakpoint in the code line below to debug your script.
-        print(f'Hi, {name}')  # Press F9 to toggle the breakpoint.
 
-class Timer_Acquirer(object):
+class TimerAcquirer(object):
 
-    def Get_sys_time(self):
+    def __init__(self):
+        pass
+
+    def get_sys_time(self):
         sys_time=time.perf_counter()
         print(f'the sys time is {sys_time} ')
 
-    def Get_Sys_time_ns(self):
+    def get_sys_time_ns(self):
         print('we begin get system time !')
         sys_ns=time.process_time_ns()
         print(f'the system time by ns is {sys_ns} !')
 
-    def Delay_One_Second(self):
+    def delay_one_second(self):
         print('we begin to delay !')
         time.sleep(1)
         print(f'the time has delayed one second')
 
-    def User_Delayer(self, seconds= None):
+    def user_delayer(self, seconds= None):
         print(f'you have defined delay {seconds} s !')
         time.sleep(seconds)
         print(f'the time has delayed {seconds} second')
 
 
-
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    Case=Hello()
-    Case.print_hi('PyCharm')
+    Delayer=TimerAcquirer()
+    Delayer.get_sys_time()
+    Delayer.get_sys_time_ns()
 
-    Delayer=Timer_Acquirer()
-    Delayer.Get_sys_time()
-    Delayer.Get_Sys_time_ns()
+
